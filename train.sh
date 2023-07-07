2b2e61266c80244eadda7565480c4e4539747761
Leon Wiemers <leonwiemers@gmail.com>
2023-07-07 09:42:54 +0200

    update train command

--- train.sh
+++ train.sh
@@ -2,5 +2,5 @@ python run.py \
  --config configs/config_diffusion_cifar10.py \
  --mode train \
  --model_dir results/cifar10 \
- --config.train.checkpoint_epochs 5 \
+ --config.train.checkpoint_epochs 1000 \
  --config.train.keep_checkpoint_max 2

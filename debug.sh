ebf659318be78849cdb1987dc64ee967f1376071
Leon W <leonwiemers@gmail.com>
2023-07-18 14:29:01 +0200

    adjust debug settings

--- debug.sh
+++ debug.sh
@@ -1,11 +1,14 @@
 rm -r results/debug
 
-WANDB_MODE=disabled python -m debugpy --wait-for-client --listen 5678 run.py \
- --run_eagerly True \
- --config configs/config_diffusion_cifar10.py \
- --mode train \
- --model_dir results/debug \
- --config.train.checkpoint_steps 1 \
- --config.train.keep_checkpoint_max 1 \
- --config.train.batch_size 8 \
- --config.train.steps 1
+export WANDB_MODE=disabled
+
+python -m debugpy --wait-for-client --listen 0.0.0.0:39203 
+    run.py \
+    --run_eagerly True \
+    --config configs/config_diffusion_cifar10.py \
+    --mode train \
+    --model_dir results/debug \
+    --config.train.checkpoint_steps 1 \
+    --config.train.keep_checkpoint_max 1 \
+    --config.train.batch_size 8 \
+    --config.train.steps 1

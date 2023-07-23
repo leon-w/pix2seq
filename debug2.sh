4fc152b61cb5a60afc7fdbad178c31432174d749
Leon W <leonwiemers@gmail.com>
2023-07-23 20:52:30 +0200

    debug utils

---
+++ debug2.sh
@@ -0,0 +1,14 @@
+rm -rf results/debug
+
+export WANDB_MODE=disabled
+
+python -m debugpy --listen 0.0.0.0:39203 \
+    run.py \
+    --run_eagerly True \
+    --config configs/config_diffusion_cifar10.py \
+    --mode train \
+    --model_dir results/debug \
+    --config.train.checkpoint_steps 1 \
+    --config.train.keep_checkpoint_max 1 \
+    --config.train.batch_size 8 \
+    --config.train.steps 1
--- debug_utils.py
+++ debug_utils.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+
 import tensorflow as tf
 from colorama import Fore, Style
-from datetime import datetime
 
 
 def format_object(o):
@@ -28,3 +29,13 @@ def p(*args, **kwargs):
     items.append(f"{k}={format_object(v)}")
 
   print(f"{Fore.GREEN}[{time}]{Style.RESET_ALL}{Fore.LIGHTRED_EX}", *items, Style.RESET_ALL)
+
+
+class CallObserver:
+  def __init__(self, obj):
+    self.name = obj.__class__.__name__
+    self.obj = obj
+
+  def __call__(self, *args, **kwargs):
+    p(f"{self.name}.__call__", *args, **kwargs)
+    return self.obj(*args, **kwargs)
